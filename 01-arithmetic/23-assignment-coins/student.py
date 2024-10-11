aa16f23e08da980e461409636ea755190c5b80db
Jonas Vanbockryck <jonasvanbockryck@gmail.com>
2024-10-11 17:21:35 +0200

    LGTM!

--- 01-arithmetic/23-assignment-coins/student.py
+++ 01-arithmetic/23-assignment-coins/student.py
@@ -1,1 +1,13 @@
 # write your code here
+
+def coins (amount):
+    c5 = amount // 5
+    amount_after_1 = amount - (5 * c5)
+
+    c2 = amount_after_1 // 2
+    amount_after_2 = amount_after_1 - (2 * c2) #made a typo here and lost 20 minutes. I love my life
+
+    c1 = amount_after_2
+
+    final_count = c5 + c2 + c1
+    return final_count
